ac2cbad4c9699055f09351d9c7d7cbbbe46b4e56
Daniel Coello <danielcoellovargas@gmail.com>
2023-07-03 17:41:50 -0500

    BottomNavigationBar
    Se agrego librería "fluentui_incons"

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 
 class BottomBar extends StatefulWidget {
@@ -19,12 +20,25 @@ class _BottomBarState extends State<BottomBar> {
             "My body"),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        elevation: 10,
+        showSelectedLabels: false, //To hide labels
+        showUnselectedLabels: false, //to hide unselected labels
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: const Color(0xFF526480),
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-          BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket), label: "Ticket"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile")
-        ],
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+              label: "Home"),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              label: "Search"),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              label: "Ticket"),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              label: "Profile")
+        ], //36,50
       ),
     );
   }
