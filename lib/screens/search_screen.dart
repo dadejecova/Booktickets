c4421a518691d7a8dc60003208cbfe1538cbe828
Daniel Coello <danielcoellovargas@gmail.com>
2023-08-17 15:58:55 -0500

    Search Screen
    new view

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,22 @@
+import 'package:booktickets/utils/app_layout.dart';
+import 'package:booktickets/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(20), vertical: AppLayout.getHeight(20)),
+        children: [
+          Gap(AppLayout.getHeight(40)),
+          Text("What are \nyou looking for?", style: Styles.headLinesStyle.copyWith(fontSize: AppLayout.getWidth(35)),)
+        ],
+      ),
+    );
+  }
+}
